ecd5c55b2fa140e43aa23e33a1a84ab8e14d9ef1
nicolas-arquiel <arquiel.nicolas@gmail.com>
2025-11-12 20:32:06 -0300

    fix

---
+++ src/main/java/com/app/config/MvcApplication.java
@@ -0,0 +1,8 @@
+package com.app.config;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class MvcApplication extends Application {
+}
